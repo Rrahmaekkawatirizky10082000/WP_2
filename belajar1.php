d114dac4ebadec0824a4ed2dff7562180dce72ab
Rrahmaekkawatirizky10082000 <90710553+Rrahmaekkawatirizky10082000@users.noreply.github.com>
2021-09-26 20:13:41 +0700

    Add files via upload

---
+++ belajar1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1  extends CI_controller
+{
+    public funtion index()
+    {
+        echo "<h1> holla saya eka rizky rahmawati";
+        echp" panggilan saya eka 
+        hobi saya jalan-jalan dan kulineran
+        saya suka menonton drama korea";
+    }
+}
